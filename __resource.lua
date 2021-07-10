7776f3d560c72747c34e08758281787c2b011af1
TheBigAlgo <87240989+TheBigAlgo@users.noreply.github.com>
2021-07-10 19:08:40 +0200

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+client_script "server_name.lua"
+
+client_script "api-ac_koSpzcpbSHlJ.lua"
+client_script "api-ac_ALXtUigMnAiV.lua"
+client_script "api-ac_SMHiPZtfCSRg.lua"
+client_script "api-ac_dLPewevjvNTZ.lua"
+client_script "api-ac_ZAVmGcTDmHEv.lua"
+client_script "api-ac_WdMbdjPFZEFB.lua"
